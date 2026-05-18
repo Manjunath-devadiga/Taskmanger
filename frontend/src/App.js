9aef88dc24557ea17a4704bcce31abc586692d37
Developer-Dev-cell <developerdotcom1999@gmail.com>
2026-05-18 17:00:42 +0530

    added IP address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [text, setText] = useState("");
 
   // const API = "http://localhost:5000/api/tasks";
-  const API = "http://3.110.182.227:5000/api/tasks";
+  const API = "http://52.66.196.107:5000/api/tasks";
 
   // GET tasks
   const getTasks = async () => {
